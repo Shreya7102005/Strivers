ee836052329ada45333ba3c1cdf8153b3041bb1a
Shreya <shreyakumari.7102005@gmail.com>
2026-06-27 11:03:28 +0530

    medium

--- maxSubarraySum.java
+++ maxSubarraySum.java
@@ -1,16 +1,11 @@
 public class maxSubarraySum {
     public int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                max = Math.max(max, sum);
-            }
+        int currentSum=nums[0];
+        int maxSum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            currentSum=Math.max(nums[i],currentSum+nums[i]);
+            maxSum=Math.max(currentSum,maxSum);
         }
-
-        return max;
+        return maxSum;
     }
 }
