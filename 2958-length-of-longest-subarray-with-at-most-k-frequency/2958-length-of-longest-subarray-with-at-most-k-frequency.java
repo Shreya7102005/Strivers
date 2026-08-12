195d1e4726c92c5a5ac7813c2e8e98dddf86cb93
Shreya Kumari <shreyakumari.7102005@gmail.com>
2026-08-12 18:43:30 +0530

    Time: 65 ms (85.94%), Space: 88.6 MB (82.34%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -7,9 +7,7 @@ class Solution {
         int n = nums.length;
 
         for (int i = 0; i < n; i++) {
-            count.put(
-                nums[i],
-                count.getOrDefault(nums[i], 0) + 1);
+            count.put(nums[i],count.getOrDefault(nums[i], 0) + 1);
 
             while (count.get(nums[i]) > k) {
                 count.put(nums[left],count.get(nums[left]) - 1);
